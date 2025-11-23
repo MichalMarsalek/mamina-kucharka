8195a9f1b1e650ee2d62e7ef8d7cb992ac43da2b
Michal Maršálek <MichalMarsalek@users.noreply.github.com>
2025-11-23 13:51:50 +0100

    redirect to closest page

--- src/routes/[edition]/[name]/+page.ts
+++ src/routes/[edition]/[name]/+page.ts
@@ -1,9 +1,20 @@
 import type { Content } from '$lib/content';
+import { redirect } from '@sveltejs/kit';
+import didYouMean from 'didyoumean';
 
 export const ssr = false;
 
 export async function load({ params, parent }: any) {
 	const data: Content = await parent();
 	const page = data.pages.find((x: any) => x.slug === params.name);
+	if (page == null) {
+		const closestSlug = didYouMean(
+			params.name,
+			data.pages.map((x) => x.slug)
+		);
+		if (closestSlug != null) {
+			redirect(308, `/${params.edition}/${encodeURIComponent(closestSlug)}`);
+		}
+	}
 	return { page };
 }
