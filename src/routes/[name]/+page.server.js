5a3b6a64da44665a769c7ef18e49d60db6a17a6d
Michal Maršálek <MichalMarsalek@users.noreply.github.com>
2025-11-23 11:20:27 +0100

    fix perma redirect

--- src/routes/[name]/+page.server.js
+++ src/routes/[name]/+page.server.js
@@ -2,5 +2,5 @@ import { redirect } from '@sveltejs/kit';
 import { defaultEdition } from '$lib/editions';
 
 export function load({ params }) {
-	redirect(308, `/${defaultEdition}/${params.name}`);
+	redirect(308, `/${defaultEdition}/${encodeURIComponent(params.name)}`);
 }
